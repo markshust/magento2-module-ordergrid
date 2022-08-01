5786765fdb955a88957416a24c728e341c489333
Mark Shust <mark@shust.com>
2022-08-01 16:54:53 -0400

    Update order_items field into export-friendly format #12

---
+++ Plugin/FormatOrderItemsExport.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace MarkShust\OrderGrid\Plugin;
+
+use Magento\Framework\Api\Search\DocumentInterface;
+use Magento\Ui\Model\Export\MetadataProvider;
+
+class FormatOrderItemsExport
+{
+    /**
+     * Place order_items data into sanitized, semicolon-delimited list for order export.
+     * @param MetadataProvider $subject
+     * @param DocumentInterface $document
+     * @param $fields
+     * @param $options
+     * @return array
+     */
+    public function beforeGetRowData(
+        MetadataProvider $subject,
+        DocumentInterface $document,
+        $fields,
+        $options
+    ): array
+    {
+        if ($orderItems = $document->getData('order_items')) {
+            $decodedItems = html_entity_decode($orderItems);
+            $explodedItems = explode('</div><div>', $decodedItems);
+
+            foreach ($explodedItems as $itemId => $explodedItem) {
+                $explodedItems[$itemId] = trim(strip_tags($explodedItem));
+            }
+
+            $implodedItems = implode(';', $explodedItems);
+            $document->setData('order_items', $implodedItems);
+        }
+
+        return [
+            $document,
+            $fields,
+            $options,
+        ];
+    }
+}
